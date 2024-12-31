f6d35326fc694ffb453af14fa82f36a516a6584c
Irfan <irfansswnt@gmail.com>
2024-12-31 16:15:27 +0700

    s

--- prisma/seeders/appConfig.seeder.js
+++ prisma/seeders/appConfig.seeder.js
@@ -2,25 +2,38 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const create = async () => {
-    const appConfig1 = await prisma.appConfig.upsert({
+    const existingConfig1 = await prisma.appConfig.findUnique({
         where: { platform: 'ios' },
+    });
+
+    const appConfig1 = await prisma.appConfig.upsert({
+        where: {
+            id: existingConfig1 ? existingConfig1.id : 0,
+        },
         update: {
+            platform: 'ios',
             version: '1.0.0',
             force_update: false,
             download_url: 'https://apps.apple.com/id/app/ftl-gym/id1550542887?l=id',
-            message: 'ios config'
+            message: 'ios config',
         },
         create: {
             platform: 'ios',
             version: '1.0.0',
             force_update: false,
             download_url: 'https://apps.apple.com/id/app/ftl-gym/id1550542887?l=id',
-            message: 'ios config'
+            message: 'ios config',
         },
     });
 
-    const appConfig2 = await prisma.appConfig.upsert({
+    const existingConfig2 = await prisma.appConfig.findUnique({
         where: { platform: 'android' },
+    });
+
+    const appConfig2 = await prisma.appConfig.upsert({
+        where: {
+            id: existingConfig2 ? existingConfig2.id : 0,
+        },
         update: {
             version: '1.0.0',
             force_update: false,
